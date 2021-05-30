c79b1519128f80ed505f23cd0a06668ea12800eb
DanielDuong1998 <dvkhangnt@gmail.com>
2021-05-30 18:54:40 +0700

    bo sung them

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-}
+module.exports = async function (deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed();
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    "",
+    "10000000000000000000000000"
+    // "10000000000000000000000000"
+    // "10000000000000000000000000"
+
+  );
+};
