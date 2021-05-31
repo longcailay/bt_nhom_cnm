11e1737053ff888c2263640e4968b1b6a834c92d
LAPTOP-0SQ3UHT2\as <henry.nguyen@contemi.com>
2021-06-01 06:48:33 +0700

    Add giao dien

--- src/contracts/DaiTokenMock.sol
+++ src/contracts/DaiTokenMock.sol
@@ -1,6 +1,4 @@
-pragma solidity ^0.5.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+pragma solidity >=0.4.21 <0.6.0;
 
 contract DaiTokenMock is ERC20Mintable {
   string  public name;
@@ -11,7 +9,27 @@ contract DaiTokenMock is ERC20Mintable {
     name = "Dai Stablecoin (DAI)";
     symbol = "DAI";
     decimals = 18;
-    symbol = "CSI";
-    decimals = 10;
+  }
+}
+
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
   }
 }
