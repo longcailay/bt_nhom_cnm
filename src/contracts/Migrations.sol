8ce49f8b9bb3d1493f9e3d3e37e84a01c8f6462f
LAPTOP-0SQ3UHT2\as <henry.nguyen@contemi.com>
2021-06-13 11:13:11 +0700

    Add contact and connect to DB

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -8,15 +8,24 @@ contract Migrations {
     owner = msg.sender;
   }
 
-function upgradeAS(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
-}
-
   modifier restricted() {
     if (msg.sender == owner) _;
+string  public name;
+  string  public symbol;
+  uint256 public decimals;
   }
+
+contract DaiTokenMock is ERC20Mintable {
+  string  public name;
+  string  public symbol;
+  uint256 public decimals;
+
+  constructor() public {
+    name = "Dai Stablecoin (DAI)";
+    symbol = "DAI";
+    decimals = 18;
+  }
+}
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
